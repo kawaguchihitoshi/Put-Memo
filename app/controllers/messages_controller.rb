d5717af2f692698cf0d9d7cf69a441fa4c6d8cd6
kawaguchihitoshi <61231995+kawaguchihitoshi@users.noreply.github.com>
2020-04-24 23:27:39 +0900

    a
    a

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
     # @search = Message.ransack(params[:q])
     # @products = @search.result
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
